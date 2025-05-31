7f8e03e3601541013e9fa7e8cc025583242d12d2
Francisco Ruvalcaba C <32653773+frantizek@users.noreply.github.com>
2025-05-31 13:30:02 -0600

    Constant complexity, example 1

--- python_code_examples/constant_complexity_001.py
+++ python_code_examples/constant_complexity_001.py
@@ -4,20 +4,23 @@
 # Input: A list of integers.
 # Output: The first integer in the list or None.
 
-def return_last_element(list_of_integers: list):
+def return_last_element(list_of_integers: list) -> int | None:
     if not list_of_integers:
         return None
     else:
         return list_of_integers[-1]
 
-def return_first_element(list_of_integers: list):
+
+def return_first_element(list_of_integers: list) -> int | None:
     if not list_of_integers:
         return None
     else:
         return list_of_integers[0]
 
-def single_liner_return_last_element(list_of_integers: list):
+
+def single_liner_return_last_element(list_of_integers: list) -> int | None:
     return list_of_integers[-1] if list_of_integers else None
 
-def single_liner_return_first_element(list_of_integers: list):
+
+def single_liner_return_first_element(list_of_integers: list) -> int | None:
     return list_of_integers[0] if list_of_integers else None
